838fcacfebb4d971da148cf42d7124b7216e8c0d
Sachin Ahlawat <sachin.ahlawat101@gmail.com>
2020-03-19 15:23:30 +0530

    deploy

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // depolying 
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://newboard.herokuapp.com/");
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
 board.width = window.innerWidth;
